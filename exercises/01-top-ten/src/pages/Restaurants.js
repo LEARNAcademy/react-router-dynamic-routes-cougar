e8b685776f1a20c7d76ec0ff0f3a2ecad18a85d2
Eric Ream <Ream@Jordans-Air.localdomain>
2019-01-29 11:49:50 -0800

    first commit of top 10 restaurants

---
+++ exercises/01-top-ten/src/pages/Restaurants.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+
+import restaurants from '../store/restaurants'
+
+class Restaurants extends Component {
+  constructor(props){
+    super(props)
+    const{ match } = props
+
+    this.state={
+      restaurantsId: match.params.id
+    }
+  }
+
+  componentDidUpdate(prevProps){
+    const prevMatch = prevProps.match
+    const{ match } = this.props
+    if(match.params.id != prevMatch.params.id){
+      this.setState({restaurantsId: match.params.id})
+    }
+  }
+
+  render() {
+    const{ restaurantsId } = this.state
+    const restaurant = restaurants.find((r)=> r.id == restaurantsId)
+
+    return(
+      <div>
+        {restaurant &&
+          <div>
+            <h1>{restaurant.name}</h1>
+            <p>{restaurant.location}</p>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+export default Restaurants
---
+++ exercises/01-top-ten/src/store/restaurants.js
@@ -0,0 +1,70 @@
+const restaurants = [
+  {
+    id: 1,
+    name: 'BJs',
+    location: 'UTC',
+
+  },
+  {
+    id: 2,
+    name: 'Bubs',
+    location: 'J Street',
+
+  },
+  {
+    id: 3,
+    name: 'Chipotle',
+    location: 'Broadway',
+
+  },
+  {
+    id: 4,
+    name: 'Grape Deli',
+    location: 'Market Street',
+
+  },
+  {
+    id: 5,
+    name: 'Nektar',
+    location: 'Market Street',
+
+  },
+  {
+    id: 6,
+    name: 'Werewolf',
+    location: '4th Avenue',
+
+  },
+  {
+    id: 7,
+    name: 'Cocina 35',
+    location: '6th Avenue',
+
+  },
+  {
+    id: 8,
+    name: 'Crack Shack',
+    location: 'Kettner Blvd.',
+
+  },
+  {
+    id: 9,
+    name: 'Ironside',
+    location: 'India Street',
+
+  },
+  {
+    id: 10,
+    name: 'Rei Do Gado Brazilian',
+    location: '4th Avenue',
+
+  },
+  {
+    id: 11,
+    name: 'Home',
+    location: 'Top 10 Favorite Restaunts!',
+
+  },
+]
+
+export default restaurants
